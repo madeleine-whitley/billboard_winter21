200a90745577f248d0fd9fbaa58215d4fe8e6358
Madeleine Whitley <maddy.whitley@gmail.com>
2021-12-01 08:20:10 -0700

    added artist and songmodels

---
+++ db/migrate/20211201045533_create_billboards.rb
@@ -0,0 +1,11 @@
+class CreateBillboards < ActiveRecord::Migration[6.1]
+  def change
+    create_table :billboards do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
